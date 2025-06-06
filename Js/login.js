fa1758e13d04a58add9c1df102d414275ceee92d
admin <ffeff@mddd.comn>
2025-06-07 06:14:37 +0900

    10주차 완성본

--- Js/login.js
+++ Js/login.js
@@ -2,6 +2,7 @@ const check_input = () => {
     const loginForm = document.getElementById('login_form');
     const emailInput = document.getElementById('typeEmailX');
     const passwordInput = document.getElementById('typePasswordX');
+    const idsave_check = document.getElementById('idSaveCheck');
     alert('아이디, 패스워드를 체크합니다');
 
     let emailValue = emailInput.value.trim();
@@ -70,6 +71,81 @@ const check_input = () => {
     return false;
     }
 
+    if (!hasUpperCase || !hasLowerCase) {
+    alert('패스워드는 대소문자를 1개 이상 포함해야 합니다.');
+    return false;
+    }
+
+    // 여기에 세션 생성
+    function session_check() { //세션 검사
+    if (sessionStorage.getItem("Session_Storage_test")) {
+        alert("이미 로그인 되었습니다.");
+        location.href='../login/index_login.html'; // 로그인된 페이지로 이동
+    }
+    if(get_id) {
+    id.value = get_id;
+    check.checked = true;
+    }
+    session_check(); // 세션 유무 검사
+    }
+
+    function session_set() {
+    const email = document.getElementById('typeEmailX').value;
+    if (email) {
+        sessionStorage.setItem("Session_Storage_test", email);
+        }
+    }
+
+    function session_del() {//세션 삭제
+         if (sessionStorage) {
+            sessionStorage.removeItem("Session_Storage_test");
+            alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+        } else {
+         alert("세션 스토리지 지원 x");
+        }
+    }
+    function logout(){
+        session_del(); // 세션 삭제
+            location.href='../index.html';
+    }
+
+    // 제출
+    loginForm.submit();
+
+    if(idsave_check.checked == true) { // 아이디 체크 o
+    alert("쿠키를 저장합니다.", emailValue);
+    setCookie("id", emailValue, 1); // 1일 저장
+    alert("쿠키 값 :" + emailValue);
+    }
+
+    else{ // 아이디 체크 x
+    setCookie("id", emailValue.value, 0); //날짜를 0 - 쿠키 삭제
+    }
+
+    // 로그인 횟수 증가 함수
+    function login_count() {
+        let cnt = getCookie("login_cnt");
+        cnt = cnt ? parseInt(cnt) + 1 : 1;
+        setCookie("login_cnt", cnt, 1); // 1일 유지
+    }
+
+    // 로그아웃 횟수 증가 함수
+    function logout_count() {
+        let cnt = getCookie("logout_cnt");
+        cnt = cnt ? parseInt(cnt) + 1 : 1;
+        setCookie("logout_cnt", cnt, 1); // 1일 유지
+    }
+
+
+    function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+        const emailInput = document.getElementById('typeEmailX');
+        const idsave_check = document.getElementById('idSaveCheck');
+        let get_id = getCookie("id");
+        if(get_id) {
+        emailInput.value = get_id;
+        idsave_check.checked = true;
+        }
+    }
 
     const sanitizedPassword = check_xss(passwordInput);
     // check_xss 함수로 비밀번호 Sanitize
@@ -95,3 +171,5 @@ const check_xss = (input) => {
   }
   return sanitizedInput;
 };
+
+
