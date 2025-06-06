79f53764672eb82c5c2d0409531ec97e2de23e1f
admin <ffeff@mddd.comn>
2025-06-07 00:07:02 +0900

    4주차

---
+++ Js/search.js
@@ -0,0 +1,12 @@
+
+document.getElementById("search_button_msg").addEventListener("click", search_message);
+
+function search_message() {
+  let msg = "검색을 수행합니다!";
+  alert(msg);
+}
+
+function search_message() {
+  let msg = "검색 실패";
+  console.log(msg);
+}
